4ae7ea4c4f127b274d0da9be8fdd97daa9c01e21
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-02-11 16:17:39 -0500

    first commit

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,23 @@
-// // GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
+
+// Click start button 
+var startQuiz = document.querySelector(".img-container");
+
+// Listen for any clicks within the img-container div
+imageContainer.addEventListener("click", function(event) {
+  var element = event.target;
+
+// Starts timer 
+    // presented with question
+
+    //Present a question
+
+    //present second question
+
+    //add if statement if something is not "true" subtract time from clock
+
+    // add game over WHEN all questions are answered or the timer reaches 0
+
+    // add function to save initials and scores
+
+    // and return loop to start button 
+

a563ae808a06eb0f2f67c53b983ad0a1c4b58e80
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-02-12 09:52:44 -0500

    pseudocode and input of basic text into html

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,47 @@
 
-// Click start button 
-var startQuiz = document.querySelector(".img-container");
+// Click start button
+var startEl = document.querySelector("#start");
+var clickStart = true;
 
-// Listen for any clicks within the img-container div
-imageContainer.addEventListener("click", function(event) {
-  var element = event.target;
+function(StartQuiz);
+if (clickStart === true)
+    prompt ("")
+
+    
+
+// Listen for any clicks within the start div
+startEl.addEventListener("click", function(StartTimer) {
+  
+  
+  
+    var element = event.target;
+})
+
+// DEPENDENCIES(DOM ELEMENTS) 
+
+var counter = document.querySelector("#counter");
+var addButton = document.querySelector("#add");
+var subtractButton = document.querySelector("#subtract");
+
+// USER INTERACTIONS 
+addButton.addEventListener("click", function() {
+  if (count < 24) {
+   //UPDATE THE DATA
+    count++;
+     //DISPLAY THE UPDATED DATA 
+    counter.textContent = count;
+     //SAVE THE DATA 
+    localStorage.setItem("count", count);
+  }
+});
+
+subtractButton.addEventListener("click", function() {
+  if (count > 0) {
+    count--;
+    counter.textContent = count;
+    localStorage.setItem("count", count);
+  }
+});
 
 // Starts timer 
     // presented with question
@@ -20,4 +57,9 @@ imageContainer.addEventListener("click", function(event) {
     // add function to save initials and scores
 
     // and return loop to start button 
+
+    // DATA - this is about saving your scores at the end
+var count = localStorage.getItem("count");
+
+counter.textContent = count;
 
